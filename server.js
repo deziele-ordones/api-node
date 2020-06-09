5777ead067dde7306e0e8982a5bf715337e7aa33
Deziele Ordones <deziele.ordones@hotmail.com>
2020-06-09 17:21:11 -0300

    meu primeiro commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var port = 3000
+var app = express()
+app.get('/', function(req, res) {
+  res.json({status: 'Server is running!'})
+})
+app.listen(port, function() {
+  console.log(`Server is running at localhost:${port}`)
+})  
+ 
