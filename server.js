b75f53be84f5a5f05d9debec07d11dbe56506b8a
Deziele Ordones <deziele.ordones@hotmail.com>
2020-06-09 21:16:09 -0300

    Conectando a aplicação e criando model User

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-var express = require('express')
+const express = require('express')
+const bodyParser =require('body-parser')
+const mongoose =require('mongoose')
+
+
 var port = 3000
 var app = express()
-app.get('/', function(req, res) {
-  res.json({status: 'Server is running!'})
-})
+
 app.listen(port, function() {
   console.log(`Server is running at localhost:${port}`)
-})  
- 
+}) 
+
+mongoose.connect("mongodb+srv://api-node:14102008De.@cluster0-3voqx.mongodb.net/<dbname>?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use(bodyParser.json())
+
+const users={
+
+}
+
+const User =require("./src/models/user")
+
+app.post('/', async(req, res) =>{
+
+  const {nome,cidade, idade} = req.body
+  const user = await User.create({nome,cidade, idade})
+
+  res.json({user})
+})
+//MONGODB
+
+
+
+
